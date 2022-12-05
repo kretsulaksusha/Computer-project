da1f8da468c18676b589377cf2c75292447441be
kretsulaksusha <kretsula.pn@ucu.edu.ua>
2022-12-05 23:44:29 +0200

    computer_project

---
+++ computer_project.py
@@ -0,0 +1,49 @@
+"Recreated Itertools library"
+
+def count():
+    """
+    Documentation
+    """
+    pass
+
+
+def cycle():
+    """
+    Documentation
+    """
+    pass
+
+
+def repeat():
+    """
+    Documentation
+    """
+    pass
+
+
+def product():
+    """
+    Documentation
+    """
+    pass
+
+
+def permutations():
+    """
+    Documentation
+    """
+    pass
+
+
+def combinations():
+    """
+    Documentation
+    """
+    pass
+
+
+def combinations_with_replacement():
+    """
+    Documentation
+    """
+    pass

355f4bf6ea913f5c1c653a8822eaff2f709b5f08
kretsulaksusha <kretsula.pn@ucu.edu.ua>
2022-12-20 14:20:47 +0200

    test module improved

--- test_computer_project.py
+++ test_computer_project.py
@@ -13,10 +13,10 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual([next(gens[2]) for _ in range(3)], [2, 5, 8])
 
         # Errors
-        # self.assertRaises(TypeError, cp.count, 'a')
-        # self.assertRaises(TypeError, cp.count, [1, 2])
-        # self.assertRaises(TypeError, cp.count, (1, 2))
-        # self.assertRaises(TypeError, cp.count, {1, 2})
+        self.assertRaises(TypeError, list, cp.count('a'))
+        self.assertRaises(TypeError, list, cp.count([1, 2]))
+        self.assertRaises(TypeError, list, cp.count((1, 2)))
+        self.assertRaises(TypeError, list, cp.count({1, 2}))
 
     def test_cycle(self):
         """Count test."""
@@ -29,7 +29,7 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual([next(iter_obj[4]) for _ in range(5)], [0, 1, 0, 1, 0])
 
         # Errors
-        # self.assertRaises(TypeError, cp.cycle, 2)
+        self.assertRaises(TypeError, list, cp.cycle(2))
 
     def test_repeat(self):
         """Repeat test."""
@@ -45,6 +45,10 @@ class TestFunctions(unittest.TestCase):
         # self.assertEqual(list(cp.repeat((1, 2), 3)), [(1, 2), (1, 2), (1, 2)])
         # self.assertEqual(list(cp.repeat({1, 2}, 3)), [{1, 2}, {1, 2}, {1, 2}])
 
+        # Errors
+        # self.assertRaises(TypeError, list, cp.repeat(1, '1'))
+        # self.assertRaises(TypeError, list, cp.repeat([1, 2], 'a'))
+
     def test_product(self):
         """Product test."""
         data = [[1], [1, 2], (1, 2), {1, 2}, range(3)]
@@ -58,51 +62,54 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(list(cp.product('ABC', 'x')), [('A', 'x'), ('B', 'x'), ('C', 'x')])
 
         # Errors
-        # self.assertRaises(TypeError, cp.product, 2)
+        # self.assertRaises(TypeError, list, cp.product(2))
+        # self.assertRaises(TypeError, list, cp.product(data[1], repeat='1'))
 
     def test_permutations(self):
         """Permutations test."""
         data = [[1], [1, 2], (1, 2), {1, 2}, range(3), 'ABB', 'ABC', [1, [2], 'a']]
-        self.assertEqual(list(cp.permutation(data[0])), [(1,)])
-        self.assertEqual(list(cp.permutation(data[1])), [(1, 2), (2, 1)])
-        self.assertEqual(list(cp.permutation(data[2])), [(1, 2), (2, 1)])
-        self.assertEqual(list(cp.permutation(data[3])), [(1, 2), (2, 1)])
-        self.assertEqual(list(cp.permutation(data[4])), [(0, 1, 2), (0, 2, 1), (1, 0, 2),
+        self.assertEqual(list(cp.permutations(data[0])), [(1,)])
+        self.assertEqual(list(cp.permutations(data[1])), [(1, 2), (2, 1)])
+        self.assertEqual(list(cp.permutations(data[2])), [(1, 2), (2, 1)])
+        self.assertEqual(list(cp.permutations(data[3])), [(1, 2), (2, 1)])
+        self.assertEqual(list(cp.permutations(data[4])), [(0, 1, 2), (0, 2, 1), (1, 0, 2),
                                                          (1, 2, 0), (2, 0, 1), (2, 1, 0)])
-        self.assertEqual(list(cp.permutation(data[5])), [('A', 'B', 'B'), ('A', 'B', 'B'),
+        self.assertEqual(list(cp.permutations(data[5])), [('A', 'B', 'B'), ('A', 'B', 'B'),
                                                          ('B', 'A', 'B'), ('B', 'B', 'A'),
                                                          ('B', 'A', 'B'), ('B', 'B', 'A')])
-        self.assertEqual(list(cp.permutation(data[6])), [('A', 'B', 'C'), ('A', 'C', 'B'),
+        self.assertEqual(list(cp.permutations(data[6])), [('A', 'B', 'C'), ('A', 'C', 'B'),
                                                          ('B', 'A', 'C'), ('B', 'C', 'A'),
                                                          ('C', 'A', 'B'), ('C', 'B', 'A')])
-        self.assertEqual(list(cp.permutation(data[6], 2)), [('A', 'B'), ('A', 'C'), ('B', 'A'),
+        self.assertEqual(list(cp.permutations(data[6], 2)), [('A', 'B'), ('A', 'C'), ('B', 'A'),
                                                             ('B', 'C'), ('C', 'A'), ('C', 'B')])
-        self.assertEqual(list(cp.permutation(data[7])), [(1, [2], 'a'), (1, 'a', [2]),
+        self.assertEqual(list(cp.permutations(data[7])), [(1, [2], 'a'), (1, 'a', [2]),
                                                          ([2], 1, 'a'), ([2], 'a', 1),
                                                          ('a', 1, [2]), ('a', [2], 1)])
-        # self.assertEqual(list(cp.permutation(data[6], 1)), [('A',), ('B',), ('C',)])
+        self.assertEqual(list(cp.permutations(data[6], 1)), [('A',), ('B',), ('C',)])
 
         # Errors
-        # self.assertRaises(TypeError, cp.permutation, 2)
+        self.assertRaises(TypeError, list, cp.permutations(2))
 
 
     def test_combinations(self):
         """Combinations test."""
-        data = [[1, 2], (1, 2), {1, 'a'}, range(3), 'ABB', 'ABC', [1, [2], 'a', 'b']]
-        # self.assertEqual(list(cp.combinations(data[0], 0)), [()])
-        # self.assertEqual(list(cp.combinations(data[0], 1)), [(1,), (2,)])
-        # self.assertEqual(list(cp.combinations(data[1], 2)), [(1, 2)])
-        # self.assertEqual(list(cp.combinations(data[2], 2)), [(1, 'a')])
-        # self.assertEqual(list(cp.combinations(data[3], 2)), [(0, 1), (0, 2), (1, 2)])
-        # self.assertEqual(list(cp.combinations(data[4], 2)), [('A', 'B'), ('A', 'B'), ('B', 'B')])
-        # self.assertEqual(list(cp.combinations(data[5], 2)), [('A', 'B'), ('A', 'C'), ('B', 'C')])
-        # self.assertEqual(list(cp.combinations(data[6], 2)), [(1, [2]), (1, 'a'), (1, 'b'),
-        # ([2], 'a'), ([2], 'b'), ('a', 'b')])
-        # self.assertEqual(list(cp.combinations(data[6], 6)), [])
+        data = [[1, 2], (1, 2), [1, 'a'], range(3), 'ABB', 'ABC', [1, [2], 'a', 'b']]
+        self.assertEqual(list(cp.combinations(data[0], 0)), [()])
+        self.assertEqual(list(cp.combinations(data[0], 1)), [(1,), (2,)])
+        self.assertEqual(list(cp.combinations(data[1], 2)), [(1, 2)])
+        self.assertEqual(list(cp.combinations(data[2], 2)), [(1, 'a')])
+        self.assertEqual(list(cp.combinations(data[3], 2)), [(0, 1), (0, 2), (1, 2)])
+        self.assertEqual(list(cp.combinations(data[4], 2)), [('A', 'B'), ('A', 'B'), ('B', 'B')])
+        self.assertEqual(list(cp.combinations(data[5], 2)), [('A', 'B'), ('A', 'C'), ('B', 'C')])
+        self.assertEqual(list(cp.combinations(data[6], 2)), [(1, [2]), (1, 'a'), (1, 'b'),
+        ([2], 'a'), ([2], 'b'), ('a', 'b')])
+        self.assertEqual(list(cp.combinations(data[6], 6)), [])
 
         # Errors
-        # self.assertRaises(ValueError, cp.combinations, data[0], -1)
-        # self.assertRaises(TypeError, cp.combinations, data[0], 'a')
+        self.assertRaises(ValueError, list, cp.combinations(data[0], -1))
+        self.assertRaises(TypeError, list, cp.combinations(data[0], 'a'))
+        self.assertRaises(TypeError, list, cp.combinations(data[0], [1, 2]))
+        self.assertRaises(TypeError, list, cp.combinations(0, 1))
 
 
     def test_combinations_with_replacement(self):
@@ -123,13 +130,13 @@ class TestFunctions(unittest.TestCase):
         # [(1, 1), (1, [2]), ([2], [2])])
 
         # Error
-        # self.assertRaises(TypeError, cp.combinations_with_replacement, data[0], 'a')
-        # self.assertRaises(TypeError, cp.combinations_with_replacement, data[0], [1, 2])
-        # self.assertRaises(TypeError, cp.combinations_with_replacement, data[0], (1, 2))
-        # self.assertRaises(TypeError, cp.combinations_with_replacement, data[0], {1, 2})
-        # self.assertRaises(TypeError, cp.combinations_with_replacement, data[0][0], 2)
-        # self.assertRaises(ValueError, cp.combinations_with_replacement, data[0], -1)
-        # self.assertRaises(TypeError, cp.combinations_with_replacement, data[0], 'a')
+        # self.assertRaises(TypeError, list, cp.combinations_with_replacement(data[0], 'a'))
+        # self.assertRaises(TypeError, list, cp.combinations_with_replacement(data[0], [1, 2]))
+        # self.assertRaises(TypeError, list, cp.combinations_with_replacement(data[0], (1, 2)))
+        # self.assertRaises(TypeError, list, cp.combinations_with_replacement(data[0], {1, 2}))
+        # self.assertRaises(TypeError, list, cp.combinations_with_replacement(data[0][0], 2))
+        # self.assertRaises(ValueError, list, cp.combinations_with_replacement(data[0], -1))
+        # self.assertRaises(TypeError, list, cp.combinations_with_replacement(data[0], 'a'))
 
 
 if __name__ == '__main__':
